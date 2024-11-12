ff57ec6fb4d7161a076f026b043700de389db324
zubimendi <zubimendi@itestra.com>
2024-11-12 23:20:25 +0100

    first draft

---
+++ auto-bookmark.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('changeButton');
+    
+button.addEventListener('click', function() {
+    const label = document.getElementById("myLabel");
+    label.textContent = "Hello!";
+});
